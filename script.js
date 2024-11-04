cbb2680e6d6df359db5e1ceefb5e58d0f8d745eb
Muhammadrizkybagas <106129325+Muhammadrizkybagas@users.noreply.github.com>
2024-11-04 23:56:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,480 @@
+var quiz = {  
+ "JS": [  
+   {  
+     "id": 1,  
+     "question": "Satu milenial berapa tahun?",  
+     "options": [  
+       {  
+         "a": "100 tahun",  
+         "b": "10 tahun",  
+         "c": "1000 tahun",  
+         "d": "57 tahun"  
+       }  
+     ],  
+     "answer": "1000 tahun",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 2,  
+     "question": "Penduduk yang belum memliki pekerjaan disebut?",  
+     "options": [  
+       {  
+         "a": "Tenaga Kerja",  
+         "b": "Pengusaha",  
+         "c": "Penghasilan",
+         "d": "Pengangguran"
+       }  
+     ],  
+     "answer": "Pengangguran",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 3,  
+     "question": "Faktor yang mempengaruhi pertumbuhan penduduk manusia, kecuali...",  
+     "options": [  
+       {  
+         "a": "Migrasi",  
+         "b": "Kematian",  
+         "c": "Kelahiran",
+         "d": "Penghasilan"  
+       }  
+     ],  
+     "answer": "Penghasilan",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 4,  
+     "question": "Hitunglah hasil dari -12 x (18 +(-27)) adalah...",  
+     "options": [  
+       {  
+         "a": "-108",  
+         "b": "-216",
+         "c": "108",
+         "d": "216"  
+       }  
+     ],  
+     "answer": "108",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 5,  
+     "question": "Hasil dari 40 x 12 : (153 - 113) adalah...",  
+     "options": [  
+       {  
+         "a": "10",  
+         "b": "11",  
+         "c": "12",  
+         "d": "13"  
+       }  
+     ],  
+     "answer": "12",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 6,  
+     "question": "Perbandingan uang Aris dan Bagas adalah 5:3 apabila selisih uang mereka Rp 12.000,00 maka uang Bagas adalah?",  
+     "options": [  
+       {  
+         "a": "Rp 18.000,00",  
+         "b": "Rp 48.000,00",  
+         "c": "Rp 20.000,00",
+         "d": "Rp 6.000,00"  
+       }  
+     ],  
+     "answer": "Rp 18.000,00",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 7,  
+     "question": "Tujuan utama ditetapkannya norma dalam kehidupan bermasyarakat yakni...",  
+     "options": [  
+       {  
+         "a": "Menghindari adanya pertentangan",  
+         "b": "Membatasi hak-hak warga negara",  
+         "c": "Memberi hukuman bagi masyarakat",
+         "d": "Mengatur tata tertib kehidupan masyrakat"  
+       }  
+     ],  
+     "answer": "Mengatur tata tertib kehidupan masyrakat",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 8,  
+     "question": "berikut ini merupakan salah satu pola sikap pelajar yang merefleksikan keteladanan para tokoh perumus pancasila, kecuali...",  
+     "options": [  
+       {  
+         "a": "Hanya menghormati guru di sekolah",  
+         "b": "Menghargai pendapat teman dekat meskipun berlainan agama",  
+         "c": "Membayar jajan di kantin dengan jujur",  
+         "d": "Membantu teman dekat yang berasal dari satu daerah"
+       }  
+     ],  
+     "answer": "Hanya menghormati guru di sekolah",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 9,  
+     "question": "Lama permainan sepak bola adalah?",  
+     "options": [  
+       {  
+         "a": "2 x 25 menit",  
+         "b": "2 x 20 menit",
+         "c": "Sampai maghrib",
+         "d": "2 x 45 menit"
+       }  
+     ],  
+     "answer": "2 x 45 menit",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 10,  
+     "question": "Untuk memenangkan suatu permainan sepak bola dipelrukan?",  
+     "options": [  
+       {  
+         "a": "Kerja sama tim",  
+         "b": "Kerja sendiri",  
+         "c": "Bermain sendiri",  
+         "d": "Kerja sama dengan wasit"
+       }  
+     ],  
+     "answer": "Kerja sama tim",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 11,  
+     "question": "Nama lain dari Panitia Periapan Kemerdekaan Indinesia adalah?",  
+     "options": [  
+       {  
+         "a": "Dokoritsu Junbi Cosakai",  
+         "b": "Uzaki Chan Asobitai",  
+         "c": "Dokoritsu Junbi Inkai",
+         "d": "Chuunibyou Demo Koi Ga Shitai"  
+       }  
+     ],  
+     "answer": "Dokoritsu Junbi Inkai",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 12,  
+     "question": "Daya tahan tubuh dapat terjaga secara optimal apabila?",  
+     "options": [  
+       {  
+         "a": "Banyak makan",  
+         "b": "Bnayak tidur",  
+         "c": "Olahraga teratur",  
+         "d": "Banyak pacar"  
+       }  
+     ],  
+     "answer": "Olahraga teratur",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 13,  
+     "question": "Contoh sikap yang patut dicontoh seorang pelajar dalam kehidupan kecuali?",  
+     "options": [  
+       {  
+         "a": "Membantu orang tua",  
+         "b": "Berprilaku sopan terhadap orang yang lebih tua",  
+         "c": "Tidak membeda-bedakan teman",
+         "d": "Lebih mementingkan Instagram dari padapada membantu Ibu"
+       }  
+     ],  
+     "answer": "Lebih mementingkan Instagram dari padapada membantu Ibu",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 14,  
+     "question": "Di bawah ini adalah alat musik tiup, kecuali?",  
+     "options": [  
+       {  
+         "a": "Terompet",  
+         "b": "Clarinet",  
+         "c": "Suling",
+         "d": "Gitar"
+       }  
+     ],  
+     "answer": "Gitar",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 15,  
+     "question": "Tari Jaipong berasal dari Provinsi?",  
+     "options": [  
+       {  
+         "a": "Sumatera Selatan",  
+         "b": "Kalimantan",  
+         "c": "Sulawesi",  
+         "d": "jawa Barat"
+       }  
+     ],  
+     "answer": "Jawa Barat",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 16,  
+     "question": "Tari Piring dapat dijumpai di wilayah?",  
+     "options": [  
+       {  
+         "a": "Jawa Barat",  
+         "b": "Kalimantan",  
+         "c": "Sulawesi",  
+         "d": "Sumatera Barat"  
+       }  
+     ],  
+     "answer": "Sumatera Barat",
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 17,  
+     "question": "Makhluk hidup pemakan tanaman disebut?",  
+     "options": [  
+       {  
+         "a": "Karnivora",  
+         "b": "Omnivora",  
+         "c": "Herbivora",  
+         "d": "Insektivora"  
+       }  
+     ],  
+     "answer": "Herbivora",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 18,  
+     "question": "Hewan bertelur disebut?",  
+     "options": [  
+       {  
+         "a": "vivipar",  
+         "b": "ovipar",
+         "c": "ovovivipar",
+         "d": "ovo ovi ipar"
+       }  
+     ],  
+     "answer": "ovipar",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 19,  
+     "question": "Contoh dari hewan mamalia adalah?",  
+     "options": [  
+       {  
+         "a": "T-Rex",  
+         "b": "Ayam",
+         "c": "Ular",
+         "d": "Kucing"
+       }  
+     ],  
+     "answer": "Kucing",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 20,  
+     "question": "Benda yang di lemparkan ke atas, akan jatuh ke bumi. Peristiwa ini terjadi karena adanya gaya?",  
+     "options": [  
+       {  
+         "a": "gravitasi bumi",  
+         "b": "listrik",  
+         "c": "gesek udara",
+         "d": "pegas"  
+       }  
+     ],  
+     "answer": "gravitasi bumi",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 21,  
+     "question": "Dua buah gaya yang bekerja pada suatu benda dikatakan seimbang, jika gaya-gaya tersebut?",  
+     "options": [  
+       {  
+         "a": "sama besar dan searah",  
+         "b": "tidak sama besar dan searah",  
+         "c": "tidak sama besar dan berlawanan arah",  
+         "d": "sama besar dan berlawanan arah"  
+       }  
+     ],  
+     "answer": "sama besar dan berlawanan arah",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 22,  
+     "question": "Jika gaya yang dikerjakan pada suatu benda lebih kecil daripada gaya geseknya, maka yang terjadi adalah?",  
+     "options": [  
+       {  
+         "a": "diam",  
+         "b": "tetap akan bergerak",  
+         "c": "bergerak",
+         "d": "bergerak kemudian diam" 
+       }  
+     ],  
+     "answer": "diam",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 23,  
+     "question": "Ketentuan UUD 1945 menyatakan, bahwa lembaga negara yang memliki kewenangan untuk memberhentikan Presiden/Wakil Presiden adalah...",  
+     "options": [  
+       {  
+         "a": "MPR",  
+         "b": "DPR",  
+         "c": "MA",  
+         "d": "MK"  
+       }  
+     ],  
+     "answer": "MPR",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 24,  
+     "question": "Pembentukan pemerintah negara Indonesia dilandasi oleh Pancasila. Pernyataan tersebut terdapat dalam pembukaaan UUD 1945 akinea ke...",  
+     "options": [  
+       {  
+         "a": "1",  
+         "b": "2",  
+         "c": "3",
+         "d": "4"  
+       }  
+     ],  
+     "answer": "4",  
+     "score": 0,  
+     "status": ""  
+   },  
+   {  
+     "id": 25,  
+     "question": "Jalan cepat termasuk dalam cabang?",  
+     "options": [  
+       {  
+         "a": "Atletik",  
+         "b": "Senam",
+         "c": "Bola kecil",
+         "d": "Bola besar"  
+       }  
+     ],  
+     "answer": "Atletik",  
+     "score": 0,  
+     "status": ""  
+   }  
+ ]  
+}  
+var quizApp = function () {  
+ this.score = 0;  
+ this.qno = 1;  
+ this.currentque = 0;  
+ var totalque = quiz.JS.length;  
+ this.displayQuiz = function (cque) {  
+   this.currentque = cque;  
+   if (this.currentque < totalque) {  
+     $("#tque").html(totalque);  
+     $("#previous").attr("disabled", false);  
+     $("#next").attr("disabled", false);  
+     $("#qid").html(quiz.JS[this.currentque].id + '.');  
+     $("#question").html(quiz.JS[this.currentque].question);  
+     $("#question-options").html("");  
+     for (var key in quiz.JS[this.currentque].options[0]) {  
+       if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {  
+         $("#question-options").append(  
+           "<div class='form-check option-block'>" +  
+           "<label class='form-check-label'>" +  
+           "<input type='radio' class='form-check-input' name='option'  id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +  
+           quiz.JS[this.currentque].options[0][key] +  
+           "</span></label>"  
+         );  
+       }  
+     }  
+   }  
+   if (this.currentque <= 0) {  
+     $("#previous").attr("disabled", true);  
+   }  
+   if (this.currentque >= totalque) {  
+     $('#next').attr('disabled', true);  
+     for (var i = 0; i < totalque; i++) {  
+       this.score = this.score + quiz.JS[i].score;  
+     }  
+     return this.showResult(this.score);  
+   }  
+ }  
+ this.showResult = function (scr) {  
+   $("#result").addClass('result');  
+   $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");  
+   for (var j = 0; j < totalque; j++) {  
+     var res;  
+     if (quiz.JS[j].score == 0) {  
+       res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';  
+     } else {  
+       res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';  
+     }  
+     $("#result").append(  
+       '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +  
+       '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +  
+
+
+
+       '<div class="last-row"><b>Score:</b> &nbsp;' + res +  
+       '</div>'  
+     );  
+   }  
+ }  
+ this.checkAnswer = function (option) {  
+   var answer = quiz.JS[this.currentque].answer;  
+   option = option.replace(/</g, "&lt;")  //for <  
+   option = option.replace(/>/g, "&gt;")  //for >  
+   option = option.replace(/"/g, "&quot;")  
+   if (option == quiz.JS[this.currentque].answer) {  
+     if (quiz.JS[this.currentque].score == "") {  
+       quiz.JS[this.currentque].score = 1;  
+       quiz.JS[this.currentque].status = "correct";  
+     }  
+   } else {  
+     quiz.JS[this.currentque].status = "wrong";  
+   }  
+ }  
+ this.changeQuestion = function (cque) {  
+   this.currentque = this.currentque + cque;  
+   this.displayQuiz(this.currentque);  
+ }  
+}  
+var jsq = new quizApp();  
+var selectedopt;  
+$(document).ready(function () {  
+ jsq.displayQuiz(0);  
+ $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {  
+   //var radio = $(this).find('input:radio');  
+   $(this).prop("checked", true);  
+   selectedopt = $(this).val();  
+ });  
+});  
+$('#next').click(function (e) {  
+ e.preventDefault();  
+ if (selectedopt) {  
+   jsq.checkAnswer(selectedopt);  
+ }  
+ jsq.changeQuestion(1);  
+});  
+$('#previous').click(function (e) {  
+ e.preventDefault();  
+ if (selectedopt) {  
+   jsq.checkAnswer(selectedopt);  
+ }  
+ jsq.changeQuestion(-1);  
+});
